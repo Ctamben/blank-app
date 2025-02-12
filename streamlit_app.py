344740ace6408a6016622e4021b4422a4f02e596
Cristóbal Tamayo <ctamben618@g.educaand.es>
2025-02-12 22:25:28 +0000

    Primera version

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+cantidad = st.slider("Elige una cantidad")
+
+st.write(f'la cantidad seleccionada es {cantidad}')
+
+for i  in range(cantidad):
+    st.button(f'{i}')
